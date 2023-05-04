2de4e51e8f9daadb0d1beb7ecae1b0a4778b2344
Anderson Sanches <andycds@gmail.com>
2023-05-03 21:33:58 -0300

    Ciclo de Vida dos Componentes

---
+++ src/EstacaoClimatica.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+export class EstacaoClimatica extends React.Component {
+    timer = null
+
+    state = {
+        data: null
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount")
+        this.timer = setInterval(() => {
+            console.log(new Date().toLocaleTimeString())
+            this.setState({ data: new Date().toLocaleTimeString() })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount")
+        clearInterval(this.timer)
+    }
+
+    componentDidUpdate() {
+        console.log("componentDidUpdate")
+    }
+
+    render() {
+        console.log('render')
+        return (
+            <div className="card">
+                {/* o corpo do cartão */}
+                <div className='card-body'>
+                    {/* centraliza verticalmente, margem abaixo */}
+                    <div className='d-flex align-items-center border rounded mb-2' style={{ height: '6rem' }}>
+                        {/* ícone obtido do estado do componente */}
+                        <i className={`fas fa-5x ${this.props.icone}`}></i>
+                        {/* largura 75%, margem a esquerda (start), fs aumenta a fonte */}
+                        <p className='w-75 ms-3 text-center fs-1'>{this.props.estacao}</p>
+                    </div>
+                    <div>
+                        <p className='text-center'>
+                            {/* renderização condicional */}
+                            {
+                                this.props.latitude ?
+                                    `Coordenadas: ${this.props.latitude}, ${this.props.longitude}. Data: ${this.state.data}`
+                                    :
+                                    'Clique no botão para saber a sua estação climática'
+                            }
+                        </p>
+                    </div>
+                    {/* botão azul (outline, 100% de largura e margem acima) */}
+                    <button onClick={this.props.obterLocalizacao} className='btn btn-outline-primary w-100 mt-2'>
+                        Qual a minha estação?
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,119 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { EstacaoClimatica } from './EstacaoClimatica'
+import Loading from './Loading'
+
+class App extends React.Component {
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     latitude: null,
+  //     longitude: null,
+  //     estacao: null,
+  //     data: null,
+  //     icone: null,
+  //     mensagemDeErro: null
+  //   }
+  // }
+
+  state = {
+    latitude: null,
+    longitude: null,
+    estacao: null,
+    data: null,
+    icone: null,
+    mensagemDeErro: null
+  }
+
+  componentDidMount() {
+    this.obterLocalizacao()
+  }
+
+  obterEstacao = (data, latitude) => {
+    const anoAtual = data.getFullYear()
+    //new Date(ano, mês(0 a 11), dia(1 a 31))
+    //21/06
+    const d1 = new Date(anoAtual, 5, 23); // inverno
+    //24/09
+    const d2 = new Date(anoAtual, 8, 24) // primavera
+    //22/12
+    const d3 = new Date(anoAtual, 11, 22) // verão
+    //21/03
+    const d4 = new Date(anoAtual, 2, 21) //outono
+    const sul = latitude < 0;
+    if (data >= d1 && data < d2)
+      return sul ? 'Inverno' : 'Verão'
+    if (data >= d2 && data < d3)
+      return sul ? 'Primavera' : 'Outono'
+    if (data >= d3 || data < d4)
+      return sul ? 'Verão' : 'Inverno'
+    return sul ? 'Outono' : 'Primavera'
+  }
+
+  icones = {
+    'Primavera': 'fa-seedling',
+    'Verão': 'fa-umbrella-beach',
+    'Outono': 'fa-tree',
+    'Inverno': 'fa-snowman'
+  }
+
+  obterLocalizacao = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      (posicao) => {
+        let data = new Date()
+        let estacao = this.obterEstacao(data, posicao.coords.latitude);
+        let icone = this.icones[estacao];
+        // console.log(icone)
+        this.setState({
+          latitude: posicao.coords.latitude,
+          longitude: posicao.coords.longitude,
+          estacao: estacao,
+          data: data.toLocaleTimeString(),
+          icone: icone
+        })
+      },
+      (erro) => {
+        console.log(erro)
+        this.setState({ mensagemDeErro: `Tente novamente mais tarde` })
+      }
+    )
+  }
+
+  render() {
+    //console.log(this.state)
+    return (
+      // responsividade, margem acima
+      <div className="container mt-2">
+        {/* uma linha, conteúdo centralizado, display é flex */}
+        <div className="row justify-content-center">
+          {/* oito colunas das doze disponíveis serão usadas para telas médias em diante */}
+          <div className="col-md-8">
+            {
+              (!this.state.latitude && !this.state.mensagemDeErro) ?
+                <Loading mensagem="Por favor, responda à solicitação de localização" />
+                :
+                this.state.mensagemDeErro ?
+                  <p className='border rounded p-2 fs-1 text-center'>É preciso dar permissão para acesso à localização. Atualize a página e tente de novo, ajustando a configuração no seu navegador.</p>
+                  :
+                  <EstacaoClimatica
+                    icone={this.state.icone}
+                    estacao={this.state.estacao}
+                    latitude={this.state.latitude}
+                    longitude={this.state.longitude}
+                    obterLocalizacao={this.obterLocalizacao}
+                  />
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root")
+)
